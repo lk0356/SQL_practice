c12dc81e21a4cbe10188e5e4f838c34855a6afa1
lk0356 <165389383+lk0356@users.noreply.github.com>
2025-10-26 18:09:42 +0000

    Start of questions 2

--- SQL-Exercises/SQL_exercise_02/2_questions.sql
+++ SQL-Exercises/SQL_exercise_02/2_questions.sql
@@ -1,17 +1,46 @@
 -- LINK : https://en.wikibooks.org/wiki/SQL_Exercises/Employee_management
 -- 2.1 Select the last name of all employees.
+SELECT * FROM Departments
+SELECT * FROM Employees
+
+SELECT LastName FROM Employees
 -- 2.2 Select the last name of all employees, without duplicates.
+SELECT DISTINCT(LastName) FROM Employees;
 -- 2.3 Select all the data of employees whose last name is "Smith".
+SELECT * FROM Employees WHERE LastName = 'Smith';
 -- 2.4 Select all the data of employees whose last name is "Smith" or "Doe".
+SELECT * FROM Employees WHERE LastName = 'Smith' OR LastName = 'Doe';
 -- 2.5 Select all the data of employees that work in department 14.
+SELECT * FROM Employees WHERE Department = 14
 -- 2.6 Select all the data of employees that work in department 37 or department 77.
+SELECT * FROM Employees WHERE Department = 37 OR Department = 77;
 -- 2.7 Select all the data of employees whose last name begins with an "S".
+SELECT * FROM Employees WHERE LastName LIKE 'S%';
 -- 2.8 Select the sum of all the departments' budgets.
+SELECT SUM(Budget)
+FROM Departments;
 -- 2.9 Select the number of employees in each department (you only need to show the department code and the number of employees).
+SELECT COUNT(SSN) AS employee_count, Department
+FROM Employees
+GROUP BY Department;
 -- 2.10 Select all the data of employees, including each employee's department's data.
+SELECT *
+FROM Employees e 
+    FULL JOIN Departments d 
+    ON e.Department = d.Code
 -- 2.11 Select the name and last name of each employee, along with the name and budget of the employee's department.
+SELECT e.Name, e.LastName, d.Budget, e.Name
+FROM Employees e 
+    FULL JOIN Departments d 
+    ON e.Department = d.Code
 -- 2.12 Select the name and last name of employees working for departments with a budget greater than $60,000.
+SELECT e.Name, e.LastName
+FROM Employees e 
+    FULL JOIN Departments d 
+    ON e.Department = d.Code
+WHERE d.Budget > 60000
 -- 2.13 Select the departments with a budget larger than the average budget of all the departments.
+
 -- 2.14 Select the names of departments with more than two employees.
 -- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
 -- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
