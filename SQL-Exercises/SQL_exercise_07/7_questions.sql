aead12b4e02be11b5902f883634cd7699ff925ab
lk0356 <165389383+lk0356@users.noreply.github.com>
2025-11-02 16:25:11 +0000

    Start and finish of 7 questions

--- SQL-Exercises/SQL_exercise_07/7_questions.sql
+++ SQL-Exercises/SQL_exercise_07/7_questions.sql
@@ -1,4 +1,20 @@
 -- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express 
+SELECT * FROM Employee
+
 -- 7.1 Who receieved a 1.5kg package?
-    -- The result is "Al Gore's Head".
+
+SELECT * FROM Package
+
+SELECT c.Name
+FROM Client c
+    INNER JOIN Package p
+        ON c.AccountNumber = p.Recipient
+WHERE p.Weight = 1.5
+
 -- 7.2 What is the total weight of all the packages that he sent?
+SELECT SUM(p.weight)
+FROM Client c
+    INNER JOIN Package p
+        ON c.AccountNumber = p.Recipient
+WHERE c.Name = 'Al Gores Head'
+GROUP BY c.Name
